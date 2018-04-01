f741fa64c0c3baf0dd53f1ce65c94a5e833a36f4
Jody Hestick <jhestick@gmail.com>
2018-04-01 17:37:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -40,6 +40,7 @@ function destructivelyRemoveElementFromEndOfArray(array){
  
  function removeElementFromEndOfArray(array){
    var sliced = array.slice(0,array.length-1)
+   return sliced
  }
  
 /* 
