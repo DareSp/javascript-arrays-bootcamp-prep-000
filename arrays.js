c46dfb400bd5b0814ea2d91984100d28cfd108e8
Jody Hestick <jhestick@gmail.com>
2018-04-01 16:59:41 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,25 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat" , "skittles"]
+function addElementToBeginningOfArray(array, element){
+  var newArray = [element, ...array]
+  return newArray
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element){
+  var newArray2 = [...array, element]
+  return newArray2
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array, index){
+  return array[index]
+}
+
